1c9982b0c958ba1c91a606f0840da79360a841d1
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-27 13:28:35 +0300

    superbowl

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(function(game) {
+      return game.result === "W";
+    });
+  
+    // Check if a winning year was found
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
